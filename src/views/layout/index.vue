5cf22cf700969d2172553634c61c15af41743085
谭鑫森 <849610435@qq.com>
2023-02-02 18:49:45 +0800

    引入naive-ui

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,9 +1,11 @@
 <template>
     <div>layout</div>
+    <n-button>Default</n-button>
+    <n-button type="primary">Default</n-button>
 </template>
 
-<script>
-
+<script setup>
+import { NButton } from "naive-ui"
 </script>
 <style lang='scss' scoped>
 
