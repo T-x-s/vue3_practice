28348d18f0d5a2fec127451859b38b15b2c76d71
谭鑫森 <849610435@qq.com>
2023-02-07 01:05:24 +0800

    layout

--- src/views/layout/Header/index.vue
+++ src/views/layout/Header/index.vue
@@ -2,7 +2,9 @@
     <div class="header">
         <div class="header-left">
             <div class="hamburger-container" @click="handleFold">
-                <img src="@/assets/icons/hamburger2.svg" alt="折叠" class="hamburger-icon" />
+                <n-icon>
+                    <Hamburger :isActive="!collapsed"></Hamburger>
+                </n-icon>
             </div>
             <div class="">
                 <n-breadcrumb>
@@ -11,31 +13,58 @@
             </div>
         </div>
         <div class="header-right">
-            <div class="search right-item">
-                <img src="@//assets/icons/search.svg" alt="搜索" class="right-icon">
-            </div>
-            <div class="github right-item" @click="handleJump">
-                <img src="@/assets/icons/github.svg" alt="github" class="right-icon">
-            </div>
-            <div class="zoom right-item" @click="toggle">
-                <img v-show="isFullscreen" src="@/assets/icons/narrow.svg" alt="缩放" class="right-icon">
-                <img v-show="!isFullscreen" src="@/assets/icons/enlarge.svg" alt="缩放" class="right-icon">
-            </div>
-            <div class="typeface right-item">
-                <img src="@/assets/icons/typeface.svg" alt="字体" class="right-icon">
-            </div>
-            <div class="avatar right-item">
-                <div class="avatar-wrapper">
-                    <img src="@/assets/images/avatar.jpg" alt="头像" class="use-avatar">
-                    <i class="icon-caret-bottom"></i>
-                </div>
-            </div>
+            <n-tooltip placement="bottom" trigger="hover">
+                <template #trigger>
+                    <div class="search right-item">
+                        <img src="@//assets/icons/search.svg" alt="搜索" class="right-icon">
+                    </div>
+                </template>
+                <span>搜索</span>
+            </n-tooltip>
+            <n-tooltip placement="bottom" trigger="hover">
+                <template #trigger>
+                    <div class="github right-item" @click="handleJump">
+                        <img src="@/assets/icons/github.svg" alt="github" class="right-icon">
+                    </div>
+                </template>
+                <span>github</span>
+            </n-tooltip>
+            <n-tooltip placement="bottom" trigger="hover">
+                <template #trigger>
+                    <div class="zoom right-item" @click="toggle">
+                        <img v-show="isFullscreen" src="@/assets/icons/narrow.svg" alt="缩放" class="right-icon">
+                        <img v-show="!isFullscreen" src="@/assets/icons/enlarge.svg" alt="缩放" class="right-icon">
+                    </div>
+                </template>
+                <span>全屏</span>
+            </n-tooltip>
+            <n-tooltip placement="bottom" trigger="hover">
+                <template #trigger>
+                    <div class="typeface right-item">
+                        <img src="@/assets/icons/typeface.svg" alt="字体" class="right-icon">
+                    </div>
+                </template>
+                <span>布局</span>
+            </n-tooltip>
+            <n-tooltip placement="bottom" trigger="hover">
+                <template #trigger>
+                    <div class="avatar right-item">
+                        <div class="avatar-wrapper">
+                            <img src="@/assets/images/avatar.jpg" alt="头像" class="use-avatar">
+                            <i class="icon-caret-bottom"></i>
+                        </div>
+                    </div>
+                </template>
+                <span>个人中心</span>
+            </n-tooltip>
+
         </div>
     </div>
 </template>
 <script setup>
 import { ref } from "vue"
 import { useFullscreen } from '@vueuse/core';
+import Hamburger from "@/components/Hamburger/index.vue"
 const emits = defineEmits(['changeCollapse'])
 /* github跳转 */
 function handleJump() {
@@ -46,9 +75,9 @@ const { isFullscreen, toggle } = useFullscreen();
 
 let collapsed = ref(false);
 
-function handleFold(){
+function handleFold() {
     collapsed.value = !collapsed.value;
-    emits('changeCollapse',collapsed);
+    emits('changeCollapse', collapsed);
 }
 </script>
 <style lang='scss' scoped>
@@ -65,12 +94,6 @@ function handleFold(){
         .hamburger-container {
             padding: 0px 15px;
             cursor: pointer;
-
-            // transition: background .3s;
-            .hamburger-icon {
-                height: 20px;
-                width: 20px;
-            }
         }
     }
 
@@ -110,14 +133,14 @@ function handleFold(){
 }
 
 .right-item {
-    padding: 0 8px;
+    padding: 0 12px;
     color: #5a5e66;
     vertical-align: text-bottom;
 }
 
 .right-icon {
-    height: 18px;
-    width: 18px;
+    height: 16px;
+    width: 16px;
     cursor: pointer;
 }
 </style>
